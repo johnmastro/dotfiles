5a7662f8b3d3cc05ec18a5d1d5b453de2838def0
John Mastro <john.b.mastro@gmail.com>
2012-02-14 20:57:51 -0800

    remove overkill git aliases

--- .zsh/git.zsh
+++
@@ -1,8 +0,0 @@
-# function from oh-my-zsh's git plugin:
-function current_branch() {
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    echo ${ref#refs/heads/}
-}
-
-alias gpull='git pull origin $(current_branch)'
-alias gpush='git push origin $(current_branch)'
