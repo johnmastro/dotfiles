393f54ae7fc440aa1e11b9264214bc5f1108526d
John Mastro <john.b.mastro@gmail.com>
2012-02-14 20:11:20 -0800

    have zsh watch for other users

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -12,3 +12,9 @@ setopt correct
 # setopt correct_all
 setopt notify
 setopt nobeep
+
+watch=all
+logcheck=60
+# %n = username, %M = hostname, %a = action, %l = tty, %T = time,
+# %W = date
+WATCHFMT="%n from %M has %a tty%l at %T %W"
