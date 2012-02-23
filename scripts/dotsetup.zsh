3aa03f4b8b78d0adcc9ab9efa9029cc38ae352e2
John Mastro <john.b.mastro@gmail.com>
2012-02-22 17:54:30 -0800

    add scripts dir and vimup.zsh

--- scripts/dotsetup.zsh
+++ scripts/dotsetup.zsh
@@ -2,10 +2,10 @@
 
 # dotsetup.zsh
 # ------------
-# creates symbolic links into my dotfiles repo and folders for vim's tmp files.
+# create symbolic links into my dotfiles repo and folders for vim's tmp files.
 
 if [[ ! -d ~/dotfiles ]]; then
-    echo "~/dotfiles not found; exiting."
+    echo "dotsetup.zsh: ~/dotfiles not found; exiting."
     exit 1
 fi
 
