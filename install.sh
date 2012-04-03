0fb8e8ce359f7cb9df1fbcde43538732ef92c589
John Mastro <john.b.mastro@gmail.com>
2012-04-02 20:18:47 -0700

    stick to bash for scripting

--- install.sh
+++ install.sh
@@ -1,21 +1,17 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-# dotsetup.zsh
-# ------------
-# creates symbolic links into my dotfiles repo and folders for vim's tmp files.
-
-if [[ ! -d ~/dotfiles ]]; then
-    echo "~/dotfiles not found; exiting."
+if [ ! -d ~/dotfiles ]; then
+    echo "~/dotfiles not found; exiting"
     exit 1
 fi
 
-function mklink() {
+mklink() {
     # create a symbolic link. if there's already a link at the given
     # destination, delete it. if there's a file there, move it (to
     # $file.bak).
-    if [[ -e $1 ]]; then
-        if [[ -e $2 ]]; then
-            if [[ -L $2 ]]; then
+    if [ -e $1 ]; then
+        if [ -e $2 ]; then
+            if [ -L $2 ]; then
                 rm $2
             else
                 local dst="$2.bak"
@@ -33,7 +29,4 @@ mklink ~/dotfiles/.inputrc ~/.inputrc
 mklink ~/dotfiles/.pylintrc ~/.pylintrc
 mklink ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-if [[ ! -d ~/.vim/tmp ]]; then mkdir -p ~/.vim/tmp; fi
-if [[ ! -d ~/.vim/tmp/backup ]]; then mkdir ~/.vim/tmp/backup; fi
-if [[ ! -d ~/.vim/tmp/undo ]]; then mkdir ~/.vim/tmp/undo; fi
-if [[ ! -d ~/.vim/tmp/swap ]]; then mkdir ~/.vim/tmp/swap; fi
+mkdir -p ~/.vim/tmp/{backup,undo,swap}
