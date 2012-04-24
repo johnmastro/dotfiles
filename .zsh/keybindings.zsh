8391981bf0657213f42cded6ce724e6ce9fcabe8
John Mastro <john.b.mastro@gmail.com>
2012-04-23 20:20:08 -0700

    add a couple zsh keybindings

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -2,6 +2,10 @@ bindkey -e
 
 bindkey -M menuselect '^o' accept-and-infer-next-history
 
+# use the up/down keys to search command history
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
+
 if [[ "$OSTYPE" == darwin* ]]; then
     bindkey "^[[H" beginning-of-line
     bindkey "^[[F" end-of-line
