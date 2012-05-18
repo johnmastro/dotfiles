34cd5c2a68cd209da94ccc40ce28d324892fa648
John Mastro <john.b.mastro@gmail.com>
2012-05-18 23:05:17 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,52 @@
 #!/usr/bin/env bash
 
-if [ ! -d ~/dotfiles ]; then
-    echo "~/dotfiles not found; exiting"
-    exit 1
+shopt -s extglob dotglob nullglob
+
+if [[ "$1" == "--force" ]]; then
+    # remove existing files/directories
+    force_install="Y"
+else
+    # skip existing files/directories (symlinks are still removed)
+    force_install="N"
 fi
 
-mklink() {
-    # create a symbolic link. if there's already a link at the given
-    # destination, delete it. if there's a file there, move it (to
-    # $file.bak).
-    if [ -e $1 ]; then
-        if [ -e $2 ]; then
-            if [ -L $2 ]; then
-                rm $2
-            else
-                local dst="$2.bak"
-                mv $2 $dst
-            fi
+repo="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mklink () {
+    local src="$1"
+    local dst="$2"
+
+    if [ -L "$dst" ]; then
+        rm -f "$dst"
+    elif [[ -f "$dst" || -d "$dst" ]]; then
+        if [[ "$force_install" == "Y" ]]; then
+            [ -d "$dst" ] && rm -rf "$dst"
+            [ -f "$dst" ] && rm -f "$dst"
+        else
+            echo -e "\twarning: $dst exists; skipping"
+            return
         fi
-        ln -s $1 $2
     fi
+    ln -s "$src" "$dst"
 }
 
-mklink ~/dotfiles/.zshrc ~/.zshrc
-mklink ~/dotfiles/.vimrc ~/.vimrc
-mklink ~/dotfiles/.gitconfig ~/.gitconfig
-mklink ~/dotfiles/.inputrc ~/.inputrc
-mklink ~/dotfiles/.pylintrc ~/.pylintrc
-mklink ~/dotfiles/.psqlrc ~/.psqlrc
-mklink ~/dotfiles/.tmux.conf ~/.tmux.conf
+dotfiles=($repo/!(install.sh|bin|.git|.gitignore))
+
+for fn in "${dotfiles[@]}"
+do
+    bn="$(basename $fn)"
+    link="$HOME/$bn"
+    mklink $fn $link
+done
+
+scripts=($repo/bin/*)
+mkdir -p $HOME/bin
+
+for fn in "${scripts[@]}"
+do
+    bn="$(basename $fn)"
+    link="$HOME/bin/$bn"
+    mklink $fn $link
+done
 
 mkdir -p ~/.tmp/vim/{backup,undo,swap}
