748e739defa8eb6ad26273027a25a1d1bf068760
John Mastro <john.b.mastro@gmail.com>
2012-05-22 21:13:45 +0000

    clean up install.sh a bit

--- install.sh
+++ install.sh
@@ -2,14 +2,6 @@
 
 shopt -s extglob dotglob nullglob
 
-if [[ "$1" == "--force" ]]; then
-    # remove existing files/directories
-    force_install="Y"
-else
-    # skip existing files/directories (symlinks are still removed)
-    force_install="N"
-fi
-
 repo="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mklink () {
@@ -17,36 +9,27 @@ mklink () {
     local dst="$2"
 
     if [ -L "$dst" ]; then
-        rm -f "$dst"
-    elif [[ -f "$dst" || -d "$dst" ]]; then
-        if [[ "$force_install" == "Y" ]]; then
-            [ -d "$dst" ] && rm -rf "$dst"
-            [ -f "$dst" ] && rm -f "$dst"
-        else
-            echo -e "\twarning: $dst exists; skipping"
-            return
-        fi
+        rm "$dst"
+    elif [ -e "$dst" ]; then
+        mv -f "$dst" "$dst.bak"
     fi
+
     ln -s "$src" "$dst"
 }
 
+mkdir -p $HOME/{code,src,bin}
+mkdir -p $HOME/.tmp/vim/{backup,undo,swap}
+
 dotfiles=($repo/!(install.sh|bin|.git|.gitignore))
 
-for fn in "${dotfiles[@]}"
-do
-    bn="$(basename $fn)"
-    link="$HOME/$bn"
-    mklink $fn $link
+for fn in "${dotfiles[@]}"; do
+    bn="$(basename "$fn")"
+    mklink "$fn" "$HOME/$bn"
 done
 
 scripts=($repo/bin/*)
-mkdir -p $HOME/bin
 
-for fn in "${scripts[@]}"
-do
-    bn="$(basename $fn)"
-    link="$HOME/bin/$bn"
-    mklink $fn $link
+for fn in "${scripts[@]}"; do
+    bn="$(basename "$fn")"
+    mklink "$fn" "$HOME/bin/$bn"
 done
-
-mkdir -p ~/.tmp/vim/{backup,undo,swap}
