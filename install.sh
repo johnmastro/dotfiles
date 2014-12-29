c64ec7a40950ac29d69494943c9d8b792ac46b5b
John Mastro <john.b.mastro@gmail.com>
2014-12-28 18:04:10 -0800

    install.sh: Confirm each action

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@ shopt -s extglob dotglob nullglob
 repo="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mklink () {
+    if [[ $# != 2 ]]; then
+        echo "mklink: Wrong number of arguments"
+        exit 1
+    fi
+
     local src="$1"
     local dst="$2"
     local today=$(date +'%Y%m%d')
 
-    if [[ -L "$dst" ]]; then
+    if [[ -L $dst ]]; then
         rm "$dst"
-    elif [[ -e "$dst" ]]; then
+    elif [[ -e $dst ]]; then
         mv -f "$dst" "$dst.bak.$today"
     fi
 
@@ -20,43 +25,70 @@ mklink () {
 
 link_dotfiles () {
     echo "Creating links to dotfiles"
-    local dotfiles=($repo/!(install.sh|bin|.git|.gitignore))
+    echo "--------------------------"
+    local dotfiles=($repo/!(.|..|install.sh|bin|.git|.gitignore))
 
-    local fn
-    local bn
-    for fn in "${dotfiles[@]}"; do
-        bn="$(basename "$fn")"
-        mklink "$fn" "$HOME/$bn"
+    local path; local name
+    for path in "${dotfiles[@]}"; do
+        name="$(basename "$path")"
+        echo "Install '"$name"'?"
+        echo -n "> "
+        read answer
+        if [[ $answer =~ [yY] ]]; then
+            mklink "$path" "$HOME/$name"
+        fi
     done
 }
 
 link_bin () {
     echo "Creating links to executables"
+    echo "-----------------------------"
     local scripts=($repo/bin/*)
 
-    local fn
-    local bn
-    for fn in "${scripts[@]}"; do
-        bn="$(basename "$fn")"
-        mklink "$fn" "$HOME/bin/$bn"
+    local path; local name
+    for path in "${scripts[@]}"; do
+        name="$(basename "$path")"
+        echo "Install '"$name"'?"
+        echo -n "> "
+        read answer
+        if [[ $answer =~ [yY] ]]; then
+            mklink "$path" "$HOME/bin/$name"
+        fi
     done
-
 }
 
 make_dirs () {
     echo "Creating directories"
-    mkdir -p $HOME/{code,src,bin,.maildir/fastmail}
+    echo "--------------------"
+
+    local path; local name
+    for name in code src bin .maildir/fastmail; do
+        path="$HOME/$name"
+        echo "Create '"$path"'?"
+        echo -n "> "
+        read answer
+        if [[ $answer =~ [yY] ]]; then
+            mkdir -p "$path"
+        fi
+    done
+}
+
+run_all () {
+    make_dirs
+    link_dotfiles
+    link_bin
 }
 
 run () {
     if [[ $# == 0 ]]; then
-        echo "Usage: install.sh [bin|dirs|dotfiles]"
+        echo "Usage: install.sh [bin|dirs|dotfiles|all]"
     fi
     for arg in "$@"; do
         case $arg in
             dirs) make_dirs;;
             dotfiles) link_dotfiles;;
             bin) link_bin;;
+            all) run_all;;
             *) echo "Unknown arg '"$arg"'; ignoring";;
         esac
     done
