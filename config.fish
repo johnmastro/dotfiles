9903a5da8f1f39b71769d63adf76f7433baaff09
John Mastro <johnmastro@mercury.com>
2024-12-31 17:13:49 -0500

    update fish config

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@
 #   - brew install fish
 #   - add it to /etc/shells
 #   - sudo chsh
-#   - ln -s ~/code/dotfiles/config.sh ~/.config/fish/config.fish
+#   - ln -s ~/code/dotfiles/config.fish ~/.config/fish/config.fish
 #   - install fisher: https://github.com/jorgebucaran/fisher
 #   - fisher install jorgebucaran/nvm.fish
 #   - fisher install IlanCosman/tide@v6
@@ -10,21 +10,12 @@
 
 if status is-interactive
     fish_add_path /opt/homebrew/bin
-    fish_add_path ~/.yarn/bin
 
-    if test -f /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-        source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-    end
+    set -gx EDITOR hx
 
-    set -gx EDITOR nvim
-    set -gx DOCKER_DEFAULT_PLATFORM linux/amd64
-
-    set -U tide_right_prompt_items \
-        status cmd_duration context jobs node virtual_env go toolbox time
-
-    abbr --add e 'nvim'
-    abbr --add g 'git'
-    abbr --add k 'kubectl'
+    abbr --add e hx
+    abbr --add g git
+    abbr --add k kubectl
 
     if test -f "$(dirname (status --current-filename))/config.local.fish"
         source "$(dirname (status --current-filename))/config.local.fish"
